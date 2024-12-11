afdcb3f5dfd5e7544ef5e5cd44875748b747bcca
Kiet <minhkiet200504@gmail.com>
2024-12-11 09:29:35 +0700

    o tao file object roi lam

---
+++ Mystery Knight/src/entity/Entity.java
@@ -0,0 +1,12 @@
+package entity;
+
+public abstract class Entity {
+    public String name;
+    public int x;
+    public int y;
+    public boolean alive;
+    public int frameCount;
+    public Entity(){
+    }
+
+}
---
+++ Mystery Knight/src/human/Player.java
@@ -0,0 +1,7 @@
+package human;
+
+public class Player extends Character{
+    public int attackSprite = 0;
+    public int NumAttack = 0;
+
+}
